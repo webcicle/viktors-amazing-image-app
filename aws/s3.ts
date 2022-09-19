54c7d8311cf93efb54bf16dfa64bff9ee556828e
websicle <viktor@uneland.com>
2022-09-19 21:17:58 +0200

    Changed env vars

--- aws/s3.ts
+++ aws/s3.ts
@@ -2,8 +2,8 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 const bucketRegion = process.env.AWS_BUCKET_REGION!;
 const bucketName = process.env.AWS_BUCKET_NAME!;
-const accessKey = process.env.AWS_ACCESS_KEY!;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY!;
+const accessKey = process.env.S3_ACCESS_KEY!;
+const secretAccessKey = process.env.S3_SECRET!;
 
 const s3 = new S3Client({
 	credentials: {
