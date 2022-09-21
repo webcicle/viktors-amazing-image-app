f6d022328c08163b669dfb37b3b2e4f9b81bf18e
websicle <89933280+webcicle@users.noreply.github.com>
2022-09-21 17:59:42 +0200

    Prisma event logging

--- prisma/client.ts
+++ prisma/client.ts
@@ -7,7 +7,7 @@ declare global {
 const prisma =
 	global.prisma ||
 	new PrismaClient({
-		log: ['query'],
+		log: [{ emit: 'event', level: 'query' }, 'info', 'error', 'warn'],
 	});
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
